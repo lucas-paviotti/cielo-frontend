660fc6fd477f8f21de09f69cc3cdeda3ebd88e9d
lucas-paviotti <paviottilucas@gmail.com>
2023-02-07 20:45:53 -0300

    Agregar sección de representantes oficiales en sobre-nosotros. Corregir estilos

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useParallax } from "react-scroll-parallax";
-import Link from "next/link";
 import { ArrowRight } from "./Icons/ArrowRight";
 
 export const CallToAction = () => {
@@ -20,7 +19,12 @@ export const CallToAction = () => {
           Si desea obtener mas información sobre la marca y sus aeronaves,
           visite su sitio web usando el siguiente enlace:
         </p>
-        <Link href="https://cirrusaircraft.com/" className="btn-primary-light">
+        <a
+          href="https://cirrusaircraft.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="btn-primary-light"
+        >
           <span>Ir al sitio</span>
           <ArrowRight
             width="0.85rem"
@@ -28,7 +32,7 @@ export const CallToAction = () => {
             color="white"
             style={{ marginLeft: "0.5rem" }}
           />
-        </Link>
+        </a>
       </div>
     </section>
   );
--- components/Sections/SobreNosotros/Representantes.js
+++ components/Sections/SobreNosotros/Representantes.js
@@ -1,10 +1,44 @@
 import CirrusBackground from "../../Waves/CirrusBackground";
+import Image from "next/image";
+import { TitleWithLine } from "../../TitleWithLine";
+import { ArrowRight } from "../../Icons/ArrowRight";
 
 const Representantes = () => {
   return (
     <div className="representantes">
       <div className="representantes__background">
         <CirrusBackground />
+      </div>
+      <div className="container representantes__content">
+        <Image
+          src={"/images/aircraft-hero-g2-vision-jet.png"}
+          width={753}
+          height={401}
+          alt="Cirrus Vision Jet"
+        />
+        <TitleWithLine title={"Representantes oficiales de Cirrus Aircraft"} />
+        <div className="main-text__container">
+          <p className="main-text">
+            Somos representantes oficiales de la empresa lider de ventas en el
+            mercado aeronautico, <b>Cirrus Aircraft</b>, desde el año{" "}
+            <b>2011</b> y el <b>Único centro de ventas y servicio autorizado</b>{" "}
+            para Argentina, Bolivia, Paraguay, Uruguay y Chile.
+          </p>
+        </div>
+        <a
+          href="https://cirrusaircraft.com/"
+          target="_blank"
+          rel="noreferrer"
+          className="btn-primary-light shadow-none"
+        >
+          <span>Ir al sitio</span>
+          <ArrowRight
+            width="0.85rem"
+            height="0.75rem"
+            color="white"
+            style={{ marginLeft: "0.5rem" }}
+          />
+        </a>
       </div>
     </div>
   );
